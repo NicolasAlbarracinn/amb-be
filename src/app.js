ba874f71bed04f0bc3444f34792335e4769abb04
Juan Pablo Lorek <jp92lorek@gmail.com>
2020-11-08 22:47:29 -0300

    feat: add error generator, error handler middleware and invalid route middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
 require('./db/mongoose');
-const userRouter = require('./routers/user');
+const express = require('express');
 const cors = require('cors');
+const AppError = require('./utils/errorHandler');
+const userRouter = require('./routers/user');
 
 const app = express();
 
@@ -10,5 +11,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(userRouter);
+
+//Handle non-existent routes
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+//Handle Error
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = app;
