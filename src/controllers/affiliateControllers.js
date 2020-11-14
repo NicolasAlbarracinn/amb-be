a9c2fd0d2dcb5b186efbb6b4fab2ab64260f7628
Juan Pablo Lorek <jp92lorek@gmail.com>
2020-11-14 13:25:25 -0300

    feat: add sort for get all affiliate controller

--- src/controllers/affiliateControllers.js
+++ src/controllers/affiliateControllers.js
@@ -6,8 +6,19 @@ const catchAsync = require('../utils/catchAsync');
 const createOne = () => {};
 exports.getOne = () => {};
 
+//posibles sort criterias parameters: "asc", "desc", "ascending", "descending", 1, or -1
+//TODO: Improve this
 exports.getAll = catchAsync(async (req, res, next) => {
-  const documents = await Affiliates.find();
+  let dbQuery;
+  const { sortFiel, sortCriteria } = req.query;
+
+  if (sortFiel && sortCriteria) {
+    dbQuery = Affiliates.find().sort({ [sortFiel]: sortCriteria });
+  } else {
+    dbQuery = Affiliates.find();
+  }
+
+  const documents = await dbQuery;
 
   if (!documents) {
     return next(new AppError('No affiliates list'), 400);
